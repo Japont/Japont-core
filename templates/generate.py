c58a89fe049763846dd2100093f9ab25ff1bb4c1
3846masa <3846masahiro+git@gmail.com>
2016-04-04 04:24:27 +0900

    check GSUB Substitution

--- templates/generate.py
+++ templates/generate.py
@@ -1,6 +1,7 @@
 from os import path
 import fontforge
 
+fontforge.setPrefs('CoverageFormatsAllowed', 1)
 chars = {{chars}}
 font = fontforge.open("{{fontpath}}")
 fontname = ""
@@ -17,6 +18,12 @@ l = len(chars)
 for i in range(0, l):
     char = chars[i]
     try:
+        possubs = font[char].getPosSub("*", "*", "*")
+        for possub in possubs:
+            if possub[1] != 'Substitution':
+                continue
+            chars.append(possub[2])
+        
         uni = font[char].unicode
         if uni != char:
             chars.append(uni)
