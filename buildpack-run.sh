309e51cae5914ede2409ba6b44118ddb2a3c7738
3846masa <3846masahiro+git@gmail.com>
2016-07-27 03:09:40 +0900

    Update

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,3 +1,6 @@
 echo "-----> git clone fonts"
-git clone --depth=1 `cat $ENV_DIR/FONTS_GIT_URL` fonts
+git clone \\
+    --depth 1 --single-branch \\
+    -b ${FONTS_GIT_BRANCH:-master} \\
+    $FONTS_GIT_URL fonts
 rm -rf fonts/.git
